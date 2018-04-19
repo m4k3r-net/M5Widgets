34986b6b871cd41e30a036deaf435003dbc45031
tobozo <tobozo@users.noreply.github.com>
2018-04-19 09:26:41 +0200

    Update for 0.1.8

--- src/M5Timer.h
+++ src/M5Timer.h
@@ -20,6 +20,8 @@
 
 */
 
+#ifndef M5_LIB_VERSION // already loaded from M5Stack
+
 #ifndef M5Timer_H
 #define M5Timer_H
 
@@ -88,4 +90,5 @@ class M5Timer {
     // actual number of timers in use
     int numTimers;
 };
+#endif
 #endif
